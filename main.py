fd689e306b32398b3f7586e4114c639073143e12
k1m3rA <36106166+k1m3rA@users.noreply.github.com>
2020-12-28 16:57:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,41 +2,47 @@ import random
 import re
 import smtplib
 import config
+from email.mime.text import MIMEText
 
 participantes={}
 correos = []
-grupos={} 
+grupos={}
+server = smtplib.SMTP(config.host,config.port)
 
-def enviar_email(msg, destinatario): #ENVÍO DE MENSAJES POR SMTP
-    server = smtplib.SMTP("smtp.gmail.com",587)
-    server.starttls()        
-    server.login(config.remitente,config.clave)
-    server.sendmail(config.remitente, destinatario,msg)
-    server.quit()
+def enviar_email(msg): #ENVÍO DE MENSAJES POR SMTP
+            
+    server.sendmail(msg['From'], msg['To'],msg.as_string().encode('utf8'))#EL ENCODE PERMITE EL ENVÍO DE CARACTERES ESPECIALES
+    
+
+def contenido_email(participantes,correos_reciben,correos_regalan):
+    texto = "Le tienes que regalar algo a "+str(participantes[correos_reciben[0]])
+    msg = MIMEText(texto.encode('utf-8'), _charset='utf-8')
+    msg['Subject'] = 'Amigo Invisible'
+    msg['From'] = config.remitente
+    msg['To'] = correos_regalan[0]
+    try:
+        enviar_email(msg)
+        print("{}({}) ya tiene amigo invisible.".format(participantes[msg['To']],msg['To']))
+    except:
+        print("Ha surgido un error, {} no tiene amigo invisible y {} no recibirá ningún regalo.".format(msg['To'],correos_reciben[0]))
+    correos_reciben.remove(correos_reciben[0])
+    correos_regalan.remove(correos_regalan[0])
 
 def repartidor(correos, participantes,grupos): #REPARTE LOS AMIGOS CON LA LISTA DE CORREOS Y PARTICIPANTES
     correos_regalan = list(correos)#SE CREAN DOS LISTAS PARA PODER DIFERENCIAR QUIEN FALTA POR REGALAR Y QUIEN FALTA POR RECIBIR
     correos_reciben = list(correos)
-
+    server.starttls()
+    server.login(config.remitente,config.clave)
     while len(correos_regalan)!=0 and len(correos_reciben)!=0:
         random.shuffle(correos_regalan)#SE MEZCLAN LAS LISTAS CADA VEZ QUE SE BORRA UN ELEMENTO DE ELLAS
         random.shuffle(correos_reciben)#SEGURO QUE SE PUEDE MODIFICAR Y OPTIMIZAR
         if grupos!=1:
             if correos_reciben[0] != correos_regalan[0] and grupos[correos_reciben[0]]!=grupos[correos_regalan[0]]:#SEGURAMENTE SE PUEDE OPTIMIZAR PARA NO REPETIR EL CODIGO
-                msg = "Le tienes que regalar algo a "+str(participantes[correos_reciben[0]])
-                destinatario = correos_regalan[0]
-                enviar_email(msg, destinatario)
-                print(msg)
-                correos_reciben.remove(correos_reciben[0])
-                correos_regalan.remove(correos_regalan[0])
+                contenido_email(participantes,correos_reciben,correos_regalan)
         else:
-            if correos_reciben[0] != correos_regalan[0]:#AQUÍ ES DONDE SE REPITE
-                msg = "Le tienes que regalar algo a "+str(participantes[correos_reciben[0]])
-                destinatario = correos_regalan[0]
-                enviar_email(msg, destinatario)
-                print(msg)
-                correos_reciben.remove(correos_reciben[0])
-                correos_regalan.remove(correos_regalan[0])
+            if correos_reciben[0] != correos_regalan[0]:
+                contenido_email(participantes,correos_reciben,correos_regalan)
+    server.quit()
 
 def es_correo_valido(correo): #COMPROBAR CORREO VALIDO
     expresion_regular = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"#PARA MAS INFO -->https://parzibyte.me/blog/2018/12/04/comprobar-correo-electronico-python/
@@ -78,10 +84,10 @@ def cantidades(msg):#NUMERO DE PARTICIPANTES
 def opciones():#ACTIVAR OPCIONES ADICIONALES (RESTRICCION DE GRUPOS, DONDE LOS MIEMBROS DE UN MISMO GRUPO NO PUEDEN SER AMIGOS)
     while True:
         print("¿Quiere restringir por grupos?")
-        restr = int(input("1.Si\n2.No\n -->"))
-        if restr==1:
+        restr = input("1.Si\n2.No\n --> ")
+        if restr=='1' or restr=='Si' or restr=='si' or restr=='s' or restr=='S' or restr=='SI':
             break
-        if restr==2:
+        if restr=='2' or restr=='No' or restr=='ni' or restr=='n' or restr=='N' or restr=='NO':
             restr=False
             break
         else:
@@ -98,12 +104,12 @@ if __name__ == "__main__":#INICIO DEL PROGRAMA E HILO PRINCIPAL
         correos, participantes, grupos = introducir_datos(num_participantes, num_grupos)
     else:
         correos, participantes = introducir_datos(num_participantes,1)#LE DAS COMO VALOR DE num_grupos 1 PARA QUE NO PIDA ESE DATO
-    #try:
-    if restr:
-        repartidor(correos,participantes,grupos)
-    else:
-        repartidor(correos, participantes,1)#LE DAS COMO VALOR DE grupos = 1 PARA QUE NO COMPARE NINGUN VALOR DE LOS GRUPOS
-    #except:
-        #print("Ha surgido un error en el envío. Por favor, inténtelo de nuevo.")
+    try:
+        if restr:
+            repartidor(correos,participantes,grupos)
+        else:
+            repartidor(correos, participantes,1)#LE DAS COMO VALOR DE grupos = 1 PARA QUE NO COMPARE NINGUN VALOR DE LOS GRUPOS
+    except:
+        print("Ha surgido un error en el envío. Por favor, inténtelo de nuevo.")
     
     pass
