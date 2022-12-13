76975307822bb221842f3af924b234e755b62d76
k1m3rA <36106166+k1m3rA@users.noreply.github.com>
2022-12-13 18:08:48 +0100

    update

--- main.py
+++ main.py
@@ -1,78 +1,85 @@
 import random
 import smtplib
-import config
+import json
+import csv
+import argparse
+from email.message import EmailMessage
 
-# Create a function that ask for the user for names and their emails
-def ask_for_names_and_emails():
-    # Clear the screen
-    print("\033c")
-    # Ask for the user for names and their emails
-    names_and_emails = {}
-    # Ask for the user for the number of names and emails
-    num_names_and_emails = int(input("¿Cuántos nombres y correos desea introducir? "))
-    # Ask for the user for the names and emails and check if the input is a valid email. If it is not valid retry until it is valid.
+# Import contacts from text file
+def import_contacts(argument):
     
-    for i in range(num_names_and_emails):
-        # Ask for the user for the name
-        name = input("Write the " + str(i+1) + "º name: ")
-        # Ask for the user for the email
-        email = input("Write the email of " + name + ": ")
-        # While it is not a valid email, retry until it is valid
-        while "@" not in email and "." not in email:
-            print("The email of " + name + " is not valid, try again.")
-            email = input("Write the email of " + name + ": ")
-        # Add the name and email to the dictionary
-        names_and_emails[name] = email
-    # Return the dictionary
-    return names_and_emails
+    with open(argument, 'r') as file:
+        try:
+            # Creates a reader object to read the file
+            reader = csv.reader(file, delimiter=':')
+        except:
+            print("Error: Invalid file format")
+            exit()
+        
+        # Creates an empty list to save changes
+        contacts = []
 
+        # Iterates each element
+        for name, email in reader:
+            # Creates a dictionary with two keys: name and email
+            contact = {'name': name, 'email': email}
 
-# Create a function that sends an email with the given parameters
-def send_email(remitente, clave, host, port, destinatario, asunto, mensaje):
+            # Adds the dictionary to list
+            contacts.append(contact)
+
+    return contacts
+
+# Send email function
+def send_email(addressee, msg):
+
     # Create a SMTP object
-    s = smtplib.SMTP(host, port)
+    s = smtplib.SMTP(config["host"], config["port"])
     # Start TLS encryption
     s.starttls()
+    
+    # Create the message
+    msg_processed = EmailMessage()
+    msg_processed.set_content(msg)
+
+    # Add custom From field
+    # with remitter name and email
+    msg_processed.add_header('From', config["name"] + ' <' + config["remitter"] + '>')
+    msg_processed.add_header('Subject', "Invisble Friend")
+
     # Login with the given credentials
-    s.login(remitente, clave)
+    s.login(config["remitter"], config["password"])
     # Send the email
-    s.sendmail(remitente, destinatario, "Subject: {}\n\n{}".format(asunto, mensaje))
+    s.sendmail(config["remitter"], addressee, msg_processed.as_string())
     # Close the connection
     s.quit()
 
+# Deliver the friends list
+def deliver(contacts):
+    # This is an example of contacts list imported by import_contacts()
+    # contactos = [{"name" : "robin", "email": "robin@gmail.com"}, {"name": "joseph", "email": "joseph@gmail.com"}, {"name": "kimera", "email": "kimera@gmail.com"}]
 
+    random.shuffle(contacts)
 
-# Create a dictionary defining random doubles with a given list without repeating items
-def create_random_doubles(list_names):
-    random_doubles = {}
-    # Shuffle list
-    random.shuffle(list_names)
-    for i in range(len(list_names)):
-        # Defines the i element of list_names as the key and the next element as the value, if the key is the last element, the value is the first element.
-        random_doubles[list_names[i]] = list_names[i+1] if i+1 < len(list_names) else list_names[0]
+    for i in range(0, len(contacts)):
+        try:
+            send_email(contacts[i]["email"], "Your Invisible Friend is " + contacts[(i+1)%len(contacts)]["name"])
+            # Show in screen a message that the email was sent successfully
+            print(contacts[i]["name"] + " has recieved the invisible friend.")
+        except smtplib.SMTPRecipientsRefused:
+            print("There was an error sending the email to " + contacts[i]["name"] + ".")
+            exit()
 
-    return random_doubles
+# Import configuration
+with open("config.json") as f:
+    config = json.load(f)
+    f.close()
 
-# Main function that calls the create_random_doubles function and pass the list of doubles.
-if __name__ == "__main__":
-    # Create a dictionary with the names and emails
-    names_and_emails = ask_for_names_and_emails()
-    # Create a list of names out of the dictionary created in the ask_for_names_and_emails function
-    names = list(names_and_emails.keys())
-    # Checks if the list is empty
-    if len(names) > 0:
-        random_doubles = create_random_doubles(names)
-        # Send an email to each key in random_doubles with a string with the value of the key as message
-        for key in random_doubles:
-            # Send an email to the key with the value of the key as message. Also check if theres an error and if there is, exit the program with message.
-            try:
-                send_email(config.remitente, config.clave, config.host, config.port, names_and_emails[key], "Amigo Invisible", "Tu amigo invisible es " + random_doubles[key])
-                # Show in screen a message that the email was sent successfully
-                print(key + " has recieved the invisible friend.")
-            except smtplib.SMTPRecipientsRefused:
-                print("There was an error sending the email to " + key + ".")
-                exit()
-    # Exit if the list is empty
-    else:
-        print("The list was empty")
-        exit()
+# Parse arguments
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-f", "--file", type=str, help="Contacts file", required=True, dest="filename")
+args = parser.parse_args()
+
+# If file arg provided launch default flow
+if args.filename:
+    deliver(import_contacts(args.filename))
